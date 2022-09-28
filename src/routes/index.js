ccb35b762854fd91a6be833827118cbd9e5b3a3d
bilal <abii.manyun@gmail.com>
2022-09-28 14:35:19 +0700

    add new

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Register from "../pages/Register";
+import PrivateRoutes from "./PrivateRoutes";
+
+function index() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route element={<Profile />} path="/profile" />
+          </Route>
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/" exact />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default index;
